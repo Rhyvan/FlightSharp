9cb0fd1207d68dd00dcce6adc4620f5302071b0c
DaniTakacs <takacs.d24@gmail.com>
2020-06-30 13:44:41 +0200

    Fixing the issues after merging

--- FlightSharpWebSite/Services/FlightApiService.cs
+++ FlightSharpWebSite/Services/FlightApiService.cs
@@ -34,27 +34,10 @@ namespace FlightSharpWebSite
 
             return flights;
         }
-
-
-        public string GetResponseAsString(string origin, string destination)
-        {
-            string baseUrl =
-                "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/cheap?destination={0}&origin={1}&currency=HUF&page=None";
-            string url = string.Format(baseUrl, destination, origin);
-            _restClient = new RestClient(url);
-            var request = new RestRequest(Method.GET);
-            request.AddHeader("x-access-token", "237f37871102101c4ec439ba6c98520e");
-            request.AddHeader("x-rapidapi-host", "travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com");
-            request.AddHeader("x-rapidapi-key", "22f98697d5mshbe41e4f988558c4p113d77jsnb304fc8f16d7");
-            IRestResponse response = _restClient.Execute(request);
-            string cont = response.Content;
-            return cont;
-        }
     }
 
     public interface IFlightApiService
     {
-        public string GetResponseAsString(string origin, string destination);
         public IEnumerable<Flight> GetFlights(string origin, string destination);
     }
 }
