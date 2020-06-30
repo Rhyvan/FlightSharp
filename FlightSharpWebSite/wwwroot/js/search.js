a972429a8be528f8b839fd471e00312fa7983be6
oliverGal-05 <galoliver05@gmail.com>
2020-06-30 17:15:18 +0200

    basic search, base html, base Get in APIController, responds with fligths data

---
+++ FlightSharpWebSite/wwwroot/js/search.js
@@ -0,0 +1,20 @@
+﻿var searchBtn = document.getElementById("search");
+
+searchBtn.onclick = function () {
+    var from = document.getElementById("from").value;
+    var to = document.getElementById("to").value;
+
+    var toSend = {
+        fromPlace: from,
+        toPlace: to
+    }
+
+    var jsonString = JSON.stringify(toSend);
+    GetReq(from, to);
+}
+
+function GetReq(fromPlace, toPlace) {
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", `api/search?origin=${fromPlace}&destination=${toPlace}`, true);
+    xmlHttp.send(null);
+}
