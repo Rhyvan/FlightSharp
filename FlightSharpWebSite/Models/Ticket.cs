b830756effc8953c33a778de59b934c83ac5786b
Rhyvan <polyakan@gmail.com>
2020-06-30 21:42:10 +0200

    Add new methods to Cart, add Ticket class

---
+++ FlightSharpWebSite/Models/Ticket.cs
@@ -0,0 +1,14 @@
+﻿namespace FlightSharpWebSite.Models
+{
+    public class Ticket
+    {
+        public int Quantity { get; set; } = 1;
+
+        public Flight Flight { get; set; }
+
+        public Ticket(Flight flight)
+        {
+            Flight = flight;
+        }
+    }
+}
