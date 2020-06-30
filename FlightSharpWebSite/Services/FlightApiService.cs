3625ecdc80df6f46a208734a6b9797a72197b448
DaniTakacs <takacs.d24@gmail.com>
2020-06-30 13:25:11 +0200

    IFlightService interface added

--- FlightSharpWebSite/Services/FlightApiService.cs
+++ FlightSharpWebSite/Services/FlightApiService.cs
@@ -8,14 +8,14 @@ using System.Linq;
 
 namespace FlightSharpWebSite
 {
-    public class FlightApiService
+    public class FlightApiService : IFlightApiService
     {
         private IRestClient _restClient;
 
         public FlightApiService()
         {
-
         }
+
         public FlightApiService(IRestClient restClient)
         {
             _restClient = restClient;
@@ -28,15 +28,16 @@ namespace FlightSharpWebSite
             var json = JObject.Parse(resp);
             var flightsJson = json["data"][destination].ToString();
             IEnumerable<Flight> flights = JsonConvert.DeserializeObject<Dictionary<string, Flight>>(flightsJson)
-               .Select(kvp => kvp.Value);
+                .Select(kvp => kvp.Value);
 
 
             return flights;
         }
 
-        public virtual string GetResponseAsString(string origin, string destination)
+        public string GetResponseAsString(string origin, string destination)
         {
-            string baseUrl = "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/cheap?destination={0}&origin={1}&currency=HUF&page=None";
+            string baseUrl =
+                "https://travelpayouts-travelpayouts-flight-data-v1.p.rapidapi.com/v1/prices/cheap?destination={0}&origin={1}&currency=HUF&page=None";
             string url = string.Format(baseUrl, destination, origin);
             _restClient = new RestClient(url);
             var request = new RestRequest(Method.GET);
@@ -45,9 +46,13 @@ namespace FlightSharpWebSite
             request.AddHeader("x-rapidapi-key", "22f98697d5mshbe41e4f988558c4p113d77jsnb304fc8f16d7");
             IRestResponse response = _restClient.Execute(request);
             string cont = response.Content;
-
             return cont;
         }
+    }
 
+    public interface IFlightApiService
+    {
+        public string GetResponseAsString(string origin, string destination);
+        public IEnumerable<Flight> GetFlights(string origin, string destination);
     }
 }
