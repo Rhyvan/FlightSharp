aa283645290abcefe697da9de77c637a274d3783
DaniTakacs <takacs.d24@gmail.com>
2020-07-01 10:40:25 +0200

    GetFlights overloads added ( currency, currency + maxPrice) IClientService interface moved to responding class IClientService individual file and Interfaces folder removed

--- FlightSharpWebSite/Services/FlightApiService.cs
+++ FlightSharpWebSite/Services/FlightApiService.cs
@@ -4,6 +4,7 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using RestSharp;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -12,7 +13,6 @@ namespace FlightSharpWebSite
     public class FlightApiService : IFlightApiService
     {
         private IClientService _client;
-
 
 
         public FlightApiService(IClientService clientService)
@@ -24,20 +24,39 @@ namespace FlightSharpWebSite
 
         public virtual IEnumerable<Flight> GetFlights(string origin, string destination)
         {
-            //var resp = GetResponseAsString(origin, destination);
             var resp = _client.GetFlights(origin, destination, "HUF");
             var json = JObject.Parse(resp);
             var flightsJson = json["data"][destination].ToString();
             IEnumerable<Flight> flights = JsonConvert.DeserializeObject<Dictionary<string, Flight>>(flightsJson)
                 .Select(kvp => kvp.Value);
-
-
             return flights;
+        }
+
+        public IEnumerable<Flight> GetFlights(string origin, string destination, string currency)
+        {
+            var resp = _client.GetFlights(origin, destination, currency);
+            var json = JObject.Parse(resp);
+            var flightsJson = json["data"][destination].ToString();
+            IEnumerable<Flight> flights = JsonConvert.DeserializeObject<Dictionary<string, Flight>>(flightsJson)
+                .Select(kvp => kvp.Value);
+            return flights;
+        }
+
+        public IEnumerable<Flight> GetFlights(string origin, string destination, string currency, int maxPrice)
+        {
+            var resp = _client.GetFlights(origin, destination, currency);
+            var json = JObject.Parse(resp);
+            var flightsJson = json["data"][destination].ToString();
+            IEnumerable<Flight> flights = JsonConvert.DeserializeObject<Dictionary<string, Flight>>(flightsJson)
+                .Select(kvp => kvp.Value);
+            return from flight in flights where flight.PriceHUF <= maxPrice select flight;
         }
     }
 
     public interface IFlightApiService
     {
         public IEnumerable<Flight> GetFlights(string origin, string destination);
+        public IEnumerable<Flight> GetFlights(string origin, string destination, string currency);
+        public IEnumerable<Flight> GetFlights(string origin, string destination, string currency, int maxPrice);
     }
 }
