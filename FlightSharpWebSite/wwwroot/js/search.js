7b40e6b911bd4a271e654057ea1cbaa2531709e1
oliverGal-05 <galoliver05@gmail.com>
2020-07-01 17:39:03 +0200

    show search result (needs refactor)

--- FlightSharpWebSite/wwwroot/js/search.js
+++ FlightSharpWebSite/wwwroot/js/search.js
@@ -1,4 +1,6 @@
 ﻿var searchBtn = document.getElementById("search");
+var divForResults = document.getElementById("showResults");
+
 
 searchBtn.onclick = function () {
     var from = document.getElementById("from").value.toUpperCase();
@@ -17,7 +19,8 @@ searchBtn.onclick = function () {
         }
 
         var jsonString = JSON.stringify(toSend);
-        GetFked(from, to, returnJson);
+        GetFked(from, to, createAndSetFlightsHTML);
+        
     }
 }
 
@@ -32,14 +35,72 @@ function GetFked(fromPlace, toPlace, callback) {
         credentials: 'same-origin'
     })
         .then(response => response.json())
-        .then(json_response => callback(json_response));
+        .then(json_response => callback(json_response))
 }
 
-const returnJson = function (arrayOfFlights)
+const createAndSetFlightsHTML = function (arrayOfFlights)
 {
+    //divForResults.innerHTML = "";
+    let table = document.createElement("table");
+    table.className = "divInputs";
+    let header = document.createElement("tr");
+
+    table.appendChild(header);
+
+    let thForAirLine = document.createElement("th");
+    thForAirLine.appendChild(document.createTextNode("AirLine"));
+    header.appendChild(thForAirLine);
+
+    let thForDestination = document.createElement("th");
+    thForDestination.appendChild(document.createTextNode("Destination"));
+    header.appendChild(thForDestination);
+
+    let thForPrice = document.createElement("th");
+    thForPrice.appendChild(document.createTextNode("Price"));
+    header.appendChild(thForPrice);
+
+    let thForDeparture = document.createElement("th");
+    thForDeparture.appendChild(document.createTextNode("Departure"));
+    header.appendChild(thForDeparture);
+
+    let thForReturn = document.createElement("th");
+    thForReturn.appendChild(document.createTextNode("Return"));
+    header.appendChild(thForReturn);
+
+    divForResults.appendChild(table);
+
+
     for (var i = 0; i < arrayOfFlights.length; i++) {
+        let nextTR = document.createElement("tr");
+        nextTR.setAttribute("jsonData", JSON.stringify(arrayOfFlights[i]));
+
+        let tdForAirLine = document.createElement("td");
+        tdForAirLine.appendChild(document.createTextNode(`${JSON.stringify(arrayOfFlights[i].airLine)}`));
+        nextTR.appendChild(tdForAirLine);
+
+        let tdForDestination = document.createElement("td");
+        tdForDestination.appendChild(document.createTextNode(`${JSON.stringify(arrayOfFlights[i].destination)}`));
+        nextTR.appendChild(tdForDestination);
+
+        let tdForPrice = document.createElement("td");
+        tdForPrice.appendChild(document.createTextNode(`${JSON.stringify(arrayOfFlights[i].priceHUF)}`));
+        nextTR.appendChild(tdForPrice);
+
+        let tdForDeparture = document.createElement("td");
+        tdForDeparture.appendChild(document.createTextNode(`${JSON.stringify(arrayOfFlights[i].departure)}`));
+        nextTR.appendChild(tdForDeparture);
+
+        let tdForReturn = document.createElement("td");
+        tdForReturn.appendChild(document.createTextNode(`${JSON.stringify(arrayOfFlights[i].return)}`));
+        nextTR.appendChild(tdForReturn);
+
+        table.appendChild(nextTR);
+
+
         JSON.stringify(arrayOfFlights[i]);
         console.log(JSON.stringify(arrayOfFlights[i]));
     }
 
 }
+
+/*return priceHUF destination departure flightNo airLine*/
