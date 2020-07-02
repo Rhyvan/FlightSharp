34ef4d7480f1daf84c096bccb71812bd5ce8e1dd
DaniTakacs <takacs.d24@gmail.com>
2020-07-02 14:08:25 +0200

    Cart.js added

---
+++ FlightSharpWebSite/wwwroot/js/cart.js
@@ -0,0 +1,38 @@
+﻿function addEventListeners(buttonsList) {
+    buttonsList.forEach(function (currentBtn) {
+        currentBtn.addEventListener('click',
+            function () {
+                var flightData = currentBtn.getAttribute("jsonData");
+                var obj = JSON.parse(flightData);
+
+                var price = obj.priceHUF;
+                var airLine = obj.airLine;
+                var departure = obj.departure;
+                var destination = obj.destination;
+                var expires = obj.expirationDate;
+                var returnDate = obj.return;
+                var flightNum = obj.flightNo;
+
+
+                var jsonToPost =
+                {
+                    "Flight": {
+                        "PriceHUF": price,
+                        "AirLine": airLine,
+                        "Return": returnDate,
+                        "Destination": destination,
+                        "FlightNo": flightNum,
+                        "ExpirationDate": expires,
+                        "Departure": departure
+                    },
+                    "Quantity": 1
+                }
+
+                makePostRequest("api/cart", JSON.stringify(jsonToPost));
+            }
+        );
+    });
+}
+
+plusButtons = document.querySelectorAll("plus");
+addEventListeners(plusButtons);
--- FlightSharpWebSite/wwwroot/js/search.js
+++ FlightSharpWebSite/wwwroot/js/search.js
@@ -2,40 +2,40 @@
 var divForResults = document.getElementById("showResults");
 let buttons;
 
-function addEventListeners(buttonsList)
-{
-    buttonsList.forEach(function (currentBtn) {
-        currentBtn.addEventListener('click', function () {
-            var flightData = currentBtn.getAttribute("jsonData");
-            var obj = JSON.parse(flightData);
+function addEventListeners(buttonsList) {
+    buttonsList.forEach(function(currentBtn) {
+        currentBtn.addEventListener('click',
+            function() {
+                var flightData = currentBtn.getAttribute("jsonData");
+                var obj = JSON.parse(flightData);
 
-            var price = obj.priceHUF;
-            var airLine = obj.airLine;
-            var departure = obj.departure;
-            var destination = obj.destination;
-            var expires = obj.expirationDate;
-            var returnDate = obj.return;
-            var flightNum = obj.flightNo;
+                var price = obj.priceHUF;
+                var airLine = obj.airLine;
+                var departure = obj.departure;
+                var destination = obj.destination;
+                var expires = obj.expirationDate;
+                var returnDate = obj.return;
+                var flightNum = obj.flightNo;
 
 
-            var jsonToPost =
-            {
-                "Flight": {
-                    "PriceHUF": price,
-                    "AirLine": airLine,
-                    "Return": returnDate,
-                    "Destination": destination,
-                    "FlightNo": flightNum,
-                    "ExpirationDate" : expires,
-                    "Departure" : departure
-                },
-                "Quantity": 1
+                var jsonToPost =
+                {
+                    "Flight": {
+                        "PriceHUF": price,
+                        "AirLine": airLine,
+                        "Return": returnDate,
+                        "Destination": destination,
+                        "FlightNo": flightNum,
+                        "ExpirationDate": expires,
+                        "Departure": departure
+                    },
+                    "Quantity": 1
+                }
+
+                makePostRequest("api/cart", JSON.stringify(jsonToPost));
             }
-
-            makePostRequest("api/cart", JSON.stringify(jsonToPost));
-        }
-        )
-    })
+        );
+    });
 }
 
 function makePostRequest(whereToSend, whatToSend)
