595f53b4e23274f925c1f6b8df640d33dfcb135f
Rhyvan <polyakan@gmail.com>
2020-09-22 13:26:40 +0200

    IFlightApiService: remove comment

--- FlightSharpWebSite/Services/Interfaces/IFlightApiService.cs
+++ FlightSharpWebSite/Services/Interfaces/IFlightApiService.cs
@@ -5,7 +5,6 @@ namespace FlightSharpWebSite.Services
 {
     public interface IFlightApiService
     {
-        //public IEnumerable<Flight> GetFlights(string origin, string destination, int maxPrice);
         public IEnumerable<Flight> GetFlights(string origin, string destination, string currency, int maxPrice);
 
     }
