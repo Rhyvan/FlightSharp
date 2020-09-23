e70086fb4e87216410299d07466ba5534727cf4b
Rhyvan <polyakan@gmail.com>
2020-09-23 13:33:49 +0200

    Clean SearchController (former APIController)

---
+++ FlightSharpWebSite/Models/SearchFlight.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightSharpWebSite.Models
+{
+    [BindProperties(SupportsGet = true)]
+    public class SearchFlight
+    {
+        [Required]
+        public string Origin { get; set; }
+        [Required]
+        public string Destination { get; set; }
+        [StringLength(3)]
+        public string Currency { get; set; } = "USD";
+        [BindProperty(Name = "price")]
+        public int MaxPrice { get; set; }
+    }
+}
---
+++ FlightSharpWebSiteTests/SearchControllerTest.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using FlightSharpWebSite.Controllers;
+using FlightSharpWebSite.Models;
+using FlightSharpWebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using NUnit.Framework;
+
+namespace FlightSharpWebSiteTests
+{
+    [TestFixture]
+    class SearchControllerTest
+    {
+        private SearchController _controller;
+        private IFlightApiService _flightApiService;
+
+        public SearchControllerTest()
+        {
+            _flightApiService = Substitute.For<IFlightApiService>();
+            _controller = new SearchController(_flightApiService);
+        }
+
+
+        [Test]
+        public void TestGetSearchedFlights_QueryReturnsNoData()
+        {
+            var origin = "a";
+            var destination = "b";
+            var currency = "USD";
+            var maxPrice = 1000;
+
+            _flightApiService.GetFlights(origin, destination, currency, maxPrice).ReturnsNull();
+
+            //var dictionary = new Dictionary<string, StringValues>();
+            //dictionary.Add("origin", new StringValues("DUB"));
+            //dictionary.Add("destination", new StringValues("BUD"));
+            //_controller.Request.Query = new QueryCollection(dictionary);
+
+            var query = new SearchFlight()
+            {
+                Origin = origin,
+                Destination = destination,
+                MaxPrice = maxPrice,
+                Currency = currency
+            };
+            var result = _controller.GetSearchedFlights(query);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<NoContentResult>(result.Result);
+        }
+    }
+}
